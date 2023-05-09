d57cb6f5841cd37fee00002565d1ee61ede38153
LETTNER <benipapapapa123@gmail.com>
2023-05-09 14:16:35 +0200

    BerechnungRechteck.py gemacht

---
+++ BerechnungRechteck.py
@@ -0,0 +1,9 @@
+l = float(input("Input the length"))
+w = float(input("Input the width"))
+
+
+perimeter = round(2*l + 2*w, 1)
+
+surface = round(l*w, 1)
+
+print(f"The Perimeter is {perimeter} and the surface is {surface}")

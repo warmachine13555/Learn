2ed4d2abb7991cd6a965ba575fdbb8d5a128837d
Lettner <benipapapapa123@gmail.com>
2023-05-10 23:34:06 +0200

    Textanalyse.py fertig gemacht

--- AndiPythonKurs/Textanalyse/Textanalyse.py
+++ AndiPythonKurs/Textanalyse/Textanalyse.py
@@ -5,10 +5,7 @@ import statistics
 #vars
 Text = []
 word_length_values = []
-min_word = 0
-max_word = 0
-min_word_value = 0
-max_word_value = 0
+
 
 def analyze_text(Text):
 
@@ -24,42 +21,27 @@ def analyze_text(Text):
             words = line.split()
             Text.extend(words)
 
+    #Anzahl Wörter
+    print(f"Der Text enthält {len(Text)} Wörter.")
+
     #Durchschnittslänge Wörter
     for length in Text:
         word_length = (len(length))
         word_length_values.append(int(word_length))
+    print(f"Die Durchschnittslänge eines Wortes is {statistics.median(word_length_values)}.")
 
     #Kürzestes Wort und die länge des Wortes
     min_word = word_length_values.index(min(word_length_values))
     min_word_value = min(word_length_values)
-    print(min_word)
-    print(min_word_value)
+    print(f"Das kürzeste Wort ist {Text[min_word]} mit der Wortlänge {min_word_value}")
 
     #Längstes Wort und die länge des Wortes
     max_word = word_length_values.index(max(word_length_values))
     max_word_value = max(word_length_values)
-    print(Text[max_word])
-    print(max(word_length_values))
-
+    print(f"Das kürzeste Wort ist {Text[max_word]} mit der Wortlänge {max_word_value}")
 
 
 
 analyze_text(Text)
 
 
-#Ausgaben
-
-print(Text)
-
-#Anzahl Wörter
-print(f"Der Text enthält {len(Text)} Wörter.")
-
-#Durchschnittslänge Wörter
-print(statistics.median(word_length_values))
-
-#Kürzestes Wort und die länge des Wortes
-print(f"Das kürzeste Wort is {min_word} mit der Wortlänge {min_word_value}")
-
-#Längstes Wort und die länge des Wortes
-print(max(word_length_values))
-print(Text[max_word])
